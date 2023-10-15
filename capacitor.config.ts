6817cc1a06d0ca7a2d407d042b40777bc21733c3
Paul Visciano <paulvisciano.dev@gmail.com>
2023-10-15 16:13:25 +0300

    Rename app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'musical-heptagons',
+  appId: 'io.ionic.musicalRealms',
+  appName: 'musical-realms',
   webDir: 'build',
   server: {
     androidScheme: 'https'
