bd59d2b856e33061b8870a1ef3877b12690fe9e0
Paul Visciano <paulvisciano.dev@gmail.com>
2023-10-16 09:17:25 +0300

    Make the toggle of animation & sound more reliable

--- src/components/ChakraPlayer.tsx
+++ src/components/ChakraPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { createRef, useEffect, useRef } from 'react';
+import React, { createRef, useEffect, useRef, useState } from 'react';
 import { ChakraInterface } from './Chakra';
 import './ChakraPlayer.css'
 import { Animation } from '@ionic/react';
@@ -10,33 +10,41 @@ import ChakraShape from './chakraShape/ChakraShape';
 import ChakraCenter from './chakraCenter/ChakraCenter';
 
 const ChakraPlayer: React.FC<{ chakra: ChakraInterface }> = ({ chakra }) => {
+    const singingBowl = new SingingBowl();
+
+    const [soundIsPlaying, setSoundIsPlaying] = useState(false);
+
     const chakraShapeRef = useRef<HTMLDivElement>(null);
     const chakraCircleRef = useRef<HTMLDivElement | null>(null);
     const rotationAnimation = useRef<Animation | null>(null);
     const pulsatingAnimation = useRef<Animation | null>(null);
-    const singingBowl = new SingingBowl();
 
     useEffect(rotation(rotationAnimation, chakraShapeRef), [chakraShapeRef]);
     useEffect(pulsating(pulsatingAnimation, chakraCircleRef), [chakraCircleRef]);
 
-    const toggleAnimation = () => {
-        rotationAnimation.current?.isRunning() ? rotationAnimation.current?.pause() : rotationAnimation.current?.play();
-        pulsatingAnimation.current?.isRunning() ? pulsatingAnimation.current?.pause() : pulsatingAnimation.current?.play();
-    };
+    const [play, { pause }] = useSound(singingBowl.getSoundPath(chakra.note, MethodEnum.Glide), { volume: 0.2 });
 
-    const [glideSoundBowl] = useSound(singingBowl.getSoundPath(chakra.note, MethodEnum.Glide), { volume: 0.2 });
+    const toggle = () => {
+        if (soundIsPlaying) {
+            rotationAnimation.current?.pause();
+            pulsatingAnimation.current?.pause();
+            pause();
+            setSoundIsPlaying(false);
+        } else {
+            rotationAnimation.current?.play();
+            pulsatingAnimation.current?.play();
+            play();
+            setSoundIsPlaying(true);
+        }
+    };
 
     return (
         <ChakraShape reference={chakraShapeRef} chakra={chakra}>
-            <div ref={chakraCircleRef} className={`${chakra.nameAsString}-player`}
-                onClick={() => {
-                    toggleAnimation();
-                    glideSoundBowl();
-                }}>
-                <ChakraCenter chakra={chakra}/>
+            <div ref={chakraCircleRef} className={`${chakra.nameAsString}-player`} onClick={toggle}>
+                <ChakraCenter chakra={chakra} />
             </div>
         </ChakraShape>
-       );
+    );
 }
 
 export default ChakraPlayer;
