fb0b7a77afe69ae410019095b298d7ae79bf9c85
Paul Visciano <paulvisciano.dev@gmail.com>
2024-01-08 03:16:09 -0500

    Add strings and piano

--- src/pages/realms/athmospheresDawdio/AthmospheresDawdio.tsx
+++ src/pages/realms/athmospheresDawdio/AthmospheresDawdio.tsx
@@ -1,12 +1,25 @@
 import { Chakra, ChakraEnum, ChakraInterface } from "components/Chakra";
 import ChakraPlayer from "components/chakraPlayer/ChakraPlayer";
-import { AvailableVocals } from "components/vocalsPlayer/VocalsPlayer";
+import VocalsPlayer, { AvailableVocals } from "components/vocalsPlayer/VocalsPlayer";
 import { PositionEnum } from "positions/PositionsEnum";
 import { EffectCube, Pagination } from "swiper/modules";
 import { SwiperSlide, Swiper } from "swiper/react";
 import { Swiper as SwiperType } from 'swiper/types';
 import "./AthmospheresDawdio.css";
 import { AthmosphereVocals } from "./vocals/AthmosphereVocals";
+import { IonCol, IonGrid, IonLabel, IonRow } from "@ionic/react";
+
+import celloLowGm from "./instruments/Cello_Low_Gm.wav";
+import cello_Mid_High_Gm from "./instruments/Cello_Mid_High_Gm.wav";
+import cello_Full_Gm from "./instruments/Cello_Full_Gm.wav";
+import cello_Ensemble_Gm from "./instruments/Cello_Ensemble_Gm.wav";
+
+import violinGm from "./instruments/Violin_Gmin.wav";
+
+import piano_Peace_Gm from "./instruments/Piano_Peace_Gm.wav";
+import piano_Uplifting_Gm from "./instruments/Piano_Uplifting_Gm.wav";
+import piano_Main_Gm from "./instruments/Piano_Main_Gm.wav";
+
 
 const AthmospheresDawdioRealm: React.FC = () => {
 
@@ -22,10 +35,10 @@ const AthmospheresDawdioRealm: React.FC = () => {
   let throatChakra = new Chakra(ChakraEnum.Throat);
   throatChakra.position = PositionEnum.None;
 
-  
+
   return (<>
     <Swiper
-      className='instrument-swiper'
+      className='athmosphere-swiper'
       effect={'cube'}
       grabCursor={true}
       cubeEffect={{
@@ -50,15 +63,41 @@ const AthmospheresDawdioRealm: React.FC = () => {
       </SwiperSlide>
 
       <SwiperSlide>
-       <ChakraPlayer key={`${thirdEyeChakra.name}_${thirdEyeChakra.note}`} vocals={AthmosphereVocals.DeepAm} chakra={thirdEyeChakra} />
+        <ChakraPlayer key={`${thirdEyeChakra.name}_${thirdEyeChakra.note}`} vocals={AthmosphereVocals.DeepAm} chakra={thirdEyeChakra} />
       </SwiperSlide>
 
       <SwiperSlide>
-        <ChakraPlayer key={`${throatChakra.name}_${throatChakra.note}`} vocals={AthmosphereVocals.HeavenlyGm} chakra={throatChakra} />
+        <IonGrid fixed={true}>
+          <IonRow class="ion-align-items-center">
+            <IonCol size="3">
+              <IonLabel>Strings</IonLabel>
+              <VocalsPlayer props={{ src: violinGm, chakraName: throatChakra.name }} />
+
+              <VocalsPlayer props={{ src: celloLowGm, chakraName: throatChakra.name }} />
+              <VocalsPlayer props={{ src: cello_Mid_High_Gm, chakraName: throatChakra.name }} />
+              <VocalsPlayer props={{ src: cello_Full_Gm, chakraName: throatChakra.name }} />
+              <VocalsPlayer props={{ src: cello_Ensemble_Gm, chakraName: throatChakra.name }} />
+            </IonCol>
+
+            <IonCol offset="1" size="6">
+              <ChakraPlayer key={`${throatChakra.name}_${throatChakra.note}`} vocals={AthmosphereVocals.HeavenlyGm} chakra={throatChakra} />
+            </IonCol>
+
+            <IonCol size="2">
+
+              <IonLabel>Piano</IonLabel>
+
+              <VocalsPlayer props={{ src: piano_Peace_Gm, chakraName: throatChakra.name }} />
+              <VocalsPlayer props={{ src: piano_Uplifting_Gm, chakraName: throatChakra.name }} />
+              <VocalsPlayer props={{ src: piano_Main_Gm, chakraName: throatChakra.name }} />
+
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </SwiperSlide>
 
     </Swiper>
-    
+
   </>)
 }
 
