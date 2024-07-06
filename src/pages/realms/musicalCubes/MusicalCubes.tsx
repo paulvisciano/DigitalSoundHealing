93fb8d99da710e874308b221c613eb2b92b400bb
Paul Visciano <paulvisciano.dev@gmail.com>
2024-07-06 13:34:18 -0500

    Add a 3rd cube

--- src/pages/realms/musicalCubes/MusicalCubes.tsx
+++ src/pages/realms/musicalCubes/MusicalCubes.tsx
@@ -5,13 +5,17 @@ import { MusicalCubeSounds } from "./sounds/MusicalCubeSounds";
 const MusicalCubesRealm: React.FC = () => {
   return (<>
 
-    <div style={{ marginLeft: 500, marginTop: 200 }}>
+    <div style={{ marginLeft: 300, marginTop: 300 }}>
       <div style={{ display : "inline-block", width: 200 }}>
         <MusicalCube keyzz={0} sounds={[MusicalCubeSounds.guitarB, MusicalCubeSounds.melodyB, MusicalCubeSounds.vocalsAm, MusicalCubeSounds.brassAm]} />
       </div>
 
-      <div style={{ display : "inline-block", width: 200, marginLeft : 100}}>
-        <MusicalCube keyzz={1} sounds={[MusicalCubeSounds.guitarB, MusicalCubeSounds.melodyB, MusicalCubeSounds.vocalsAm, MusicalCubeSounds.brassAm]} />
+      <div style={{ display : "inline-block", width: 200, marginLeft : 200}}>
+        <MusicalCube keyzz={1} sounds={[MusicalCubeSounds.vocalsAm, MusicalCubeSounds.melodyB, MusicalCubeSounds.vocalsAm, MusicalCubeSounds.brassAm]} />
+      </div>
+
+      <div style={{ display : "inline-block", width: 200, marginLeft : 175}}>
+        <MusicalCube keyzz={2} sounds={[MusicalCubeSounds.brassAm, MusicalCubeSounds.melodyB, MusicalCubeSounds.vocalsAm, MusicalCubeSounds.brassAm]} />
       </div>
     </div>
 
