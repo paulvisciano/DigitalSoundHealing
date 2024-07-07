af6fab7e32a8acd5673ab75f6ee188dfec1e07c7
Paul Visciano <paulvisciano.dev@gmail.com>
2024-07-07 10:45:26 -0500

    More code cleanup

--- src/components/musicalCube/MusicalCube.tsx
+++ src/components/musicalCube/MusicalCube.tsx
@@ -8,53 +8,57 @@ import { MusicalCubeSound } from 'pages/realms/musicalCubes/sounds/MusicalCubeSo
 import React from "react";
 import { CubeSide } from './cubeSide/CubeSide';
 
+interface Size {
+    height: number;
+    width: number;
+}
+
 interface CubeOptions {
     cubeId: number;
     label: string;
+    size?: Size;
     sounds: Array<MusicalCubeSound>;
 }
 
-const MusicalCube: React.FC<CubeOptions> = ({ cubeId: cubeId, label: label, sounds }) => {
+const MusicalCube: React.FC<CubeOptions> = ({ cubeId: cubeId, label: label, size = { height: 200, width: 200 }, sounds }) => {
     return (
-        <div className={`musical-cube musical-cube-${cubeId}`} style={{ display: 'inline-block' }}>
-            <p style={{ textAlign: "center" }}>{label}</p>
+        <div className={`musical-cube musical-cube-${cubeId}`}>
+            <p className='musical-cube-label'>{label}</p>
 
-            <div style={{ height: 200, width: 200 }}>
-                <Swiper
-                    className={`musical-cube-swiper-${cubeId}`}
-                    effect={'cube'}
-                    loop={true}
-                    navigation={true}
-                    cubeEffect={{
-                        shadow: true,
-                        shadowOffset: 30,
-                        shadowScale: 0.85,
-                    }}
-                    onInit={(swiper: SwiperType) => {
-                        //Force update to remove dark background from slide
-                        setTimeout(() => {
-                            swiper.update();
-                        });
-                    }}
-                    onSwiper={(swiper: SwiperType) => {
-                        //Force update to remove dark background from slide
-                        setTimeout(() => {
-                            swiper.update();
-                        });
-                    }}
-                    modules={[EffectCube, Navigation]}
-                >
-                    {
-                        sounds.map((sound, index) =>
-                            <SwiperSlide key={`cube-slide-${index}`}>
-                                <CubeSide options={{ cubeKey: cubeId, sideKey: index, sound: sound }} />
-                            </SwiperSlide>
-                        )}
+            <Swiper
+                className={`musical-cube-swiper-${cubeId}`}
+                style={{ height: size.height, width: size.width }}
+                effect={'cube'}
+                loop={true}
+                navigation={true}
+                cubeEffect={{
+                    shadow: true,
+                    shadowOffset: 30,
+                    shadowScale: 0.85,
+                }}
+                onInit={(swiper: SwiperType) => {
+                    //Force update to remove dark background from slide
+                    setTimeout(() => {
+                        swiper.update();
+                    });
+                }}
+                onSwiper={(swiper: SwiperType) => {
+                    //Force update to remove dark background from slide
+                    setTimeout(() => {
+                        swiper.update();
+                    });
+                }}
+                modules={[EffectCube, Navigation]}
+            >
+                {
+                    sounds.map((sound, index) =>
+                        <SwiperSlide key={`cube-slide-${index}`}>
+                            <CubeSide options={{ cubeKey: cubeId, sideKey: index, sound: sound }} />
+                        </SwiperSlide>
+                    )}
 
-                </Swiper>
-            </div>
+            </Swiper>
         </div>
-
     );
 }
 
--- src/pages/realms/musicalCubes/MusicalCubesRealm.tsx
+++ src/pages/realms/musicalCubes/MusicalCubesRealm.tsx
@@ -1,17 +1,24 @@
 import "./MusicalCubesRealm.css";
 import MusicalCube from "components/musicalCube/MusicalCube";
 import { MusicalCubeSound } from "./sounds/MusicalCubeSounds";
+import { IonCol, IonGrid, IonRow } from "@ionic/react";
 
 const MusicalCubesRealm: React.FC = () => {
-  return (<>
-    <div>
-      <MusicalCube cubeId={0} label="Guitar" sounds={[MusicalCubeSound.guitarB, MusicalCubeSound.melodyB, MusicalCubeSound.vocalsAm, MusicalCubeSound.brassAm]} />
-
-      <MusicalCube cubeId={1} label="Vocals" sounds={[MusicalCubeSound.vocalsAm, MusicalCubeSound.melodyB, MusicalCubeSound.vocalsAm, MusicalCubeSound.brassAm]} />
-
-      <MusicalCube cubeId={2} label="Brass" sounds={[MusicalCubeSound.brassAm, MusicalCubeSound.melodyB, MusicalCubeSound.vocalsAm, MusicalCubeSound.brassAm]} />
-    </div>
-  </>)
+  return (
+    <IonGrid class="musical-realm">
+      <IonRow class="ion-align-items-center">
+        <IonCol>
+          <MusicalCube cubeId={1} label="Guitar" sounds={[MusicalCubeSound.guitarB, MusicalCubeSound.melodyB, MusicalCubeSound.vocalsAm, MusicalCubeSound.brassAm]} />
+        </IonCol>
+        <IonCol>
+          <MusicalCube cubeId={2} label="Vocals" sounds={[MusicalCubeSound.vocalsAm, MusicalCubeSound.melodyB, MusicalCubeSound.vocalsAm, MusicalCubeSound.brassAm]} />
+        </IonCol>
+        <IonCol>
+          <MusicalCube cubeId={3} label="Brass" sounds={[MusicalCubeSound.brassAm, MusicalCubeSound.melodyB, MusicalCubeSound.vocalsAm, MusicalCubeSound.brassAm]} />
+        </IonCol>
+      </IonRow>
+    </IonGrid>
+  )
 }
 
 export default MusicalCubesRealm;  
