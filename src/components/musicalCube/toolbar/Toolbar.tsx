84b5b8e01d65421837ce6c187ff40dd2f1dd0de9
Paul Visciano <paulvisciano.dev@gmail.com>
2024-07-11 00:14:25 -0500

    Make the toolbar display vertically

--- src/components/musicalCube/toolbar/Toolbar.tsx
+++ src/components/musicalCube/toolbar/Toolbar.tsx
@@ -1,10 +1,17 @@
-import { IonIcon } from "@ionic/react";
-import { reloadCircleSharp, reloadCircleOutline } from "ionicons/icons";
+import { IonGrid, IonIcon, IonRow } from "@ionic/react";
+import { reloadCircleSharp, reloadCircleOutline, syncCircle } from "ionicons/icons";
 import './Toolbar.css'
 
-const CubeSideToolbar: React.FC<{ loop: boolean, setLoop: any }> = ({ loop, setLoop }) => {
+const CubeSideToolbar: React.FC<{ loop: boolean, setLoop: any, triggerSync: any }> = ({ loop, setLoop, triggerSync }) => {
     return (<div className="cube-side-toolbar">
-        {loop ? <IonIcon size='large' icon={reloadCircleSharp} onClick={() => setLoop(false)} /> : <IonIcon size='large' icon={reloadCircleOutline} onClick={() => setLoop(true)} />}
+        <IonGrid>
+            <IonRow>
+                {loop ? <IonIcon size='large' icon={reloadCircleSharp} onClick={() => setLoop(false)} /> : <IonIcon size='large' icon={reloadCircleOutline} onClick={() => setLoop(true)} />}
+            </IonRow>
+            <IonRow>
+                <IonIcon size='large' icon={syncCircle} onClick={() => triggerSync()} />
+            </IonRow>
+        </IonGrid>
     </div>);
 }
 
