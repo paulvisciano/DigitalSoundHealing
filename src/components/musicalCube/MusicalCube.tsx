5e84a4f55a9a4ce25bd2faa58f866bbff2883e1c
Paul Visciano <paulvisciano.dev@gmail.com>
2024-07-23 22:02:04 -0500

    Autogen the title

--- src/components/musicalCube/MusicalCube.tsx
+++ src/components/musicalCube/MusicalCube.tsx
@@ -3,21 +3,22 @@ import './SwiperOverrides.css';
 import { SwiperSlide, Swiper } from "swiper/react";
 import { Swiper as SwiperType } from 'swiper/types';
 import { EffectCube, Navigation } from 'swiper/modules';
- 
+
 import React from "react";
 import { CubeSide } from './cubeSide/CubeSide';
 import { nanoid } from '@reduxjs/toolkit';
 import { Size } from './interfaces/Size';
 import { InTheCity } from 'pages/realms/musicalCubes/sounds/CubeSound';
+import { IonLabel } from '@ionic/react';
 
 interface Options {
-    label: string;
+    label?: string;
     size?: Size;
     sounds: Array<InTheCity>;
-    enableLoop? : boolean;
-    enableSync? : boolean;
-    setSharedTrackTime : (newTrackTime : number) => void;
-    getSharedTrackTime : () => number;
+    enableLoop?: boolean;
+    enableSync?: boolean;
+    setSharedTrackTime: (newTrackTime: number) => void;
+    getSharedTrackTime: () => number;
 }
 
 const MusicalCube: React.FC<Options> = ({ label: label, size = { height: 333, width: 333 }, sounds, ...props }) => {
@@ -53,6 +54,8 @@ const MusicalCube: React.FC<Options> = ({ label: label, size = { height: 333, wi
                 {
                     sounds.map((sound, index) =>
                         <SwiperSlide key={`cube-slide-${index}`}>
+                            <div className='cube-slide-title'>{Object.keys(InTheCity)[Object.values(InTheCity).indexOf(sound)].replaceAll('_', ' ')}</div>
+                            
                             <CubeSide id={nanoid()} size={size} sound={sound} {...props} />
                         </SwiperSlide>
                     )}
