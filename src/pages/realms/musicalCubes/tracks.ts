b4c20421141e9d05a33a813e0124e680b61987e7
Paul Visciano <paulvisciano.dev@gmail.com>
2024-08-04 22:18:08 -0400

    Tweak the order of the sounds

--- src/pages/realms/musicalCubes/tracks.ts
+++ src/pages/realms/musicalCubes/tracks.ts
@@ -7,11 +7,11 @@ export interface Track {
 const tracks : Track[] = [
       { 
         name : "Best Feelings",
-        sounds : ["./assets/sounds/musicalCube/tracks/Best Feelings/melody/AHA_SV_75_brass_loop_best_feelings_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/melody/AHA_SV_75_keys_loop_best_feelings_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/melody/AHA_SV_75_piano_loop_day_to_day_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/melody/AHA_SV_75_vocal_loop_best_feelings_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/one_shots/AHA_SV_brass_stack_one_shot_phrase_runner_up_F.wav"]
+        sounds : ["./assets/sounds/musicalCube/tracks/Best Feelings/melody/001_AHA_SV_75_piano_loop_day_to_day_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/melody/002_AHA_SV_75_keys_loop_best_feelings_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/melody/003_AHA_SV_75_brass_loop_best_feelings_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/melody/004_AHA_SV_75_vocal_loop_best_feelings_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/one_shots/AHA_SV_brass_stack_one_shot_phrase_runner_up_F.wav"]
       },
       { 
         name : "Daydreamer",
-        sounds : ["./assets/sounds/musicalCube/tracks/Daydreamer/melody/AHA_SV_70_brass_stack_loop_daydreamer_Gm.wav","./assets/sounds/musicalCube/tracks/Daydreamer/melody/AHA_SV_70_guitar_stack_loop_daydreamer_Gm.wav","./assets/sounds/musicalCube/tracks/Daydreamer/melody/AHA_SV_70_keys_loop_daydreamer_Gm.wav","./assets/sounds/musicalCube/tracks/Daydreamer/melody/AHA_SV_70_vocal_loop_daydreamer_Gm.wav"]
+        sounds : ["./assets/sounds/musicalCube/tracks/Daydreamer/melody/001_AHA_SV_70_keys_loop_daydreamer_Gm.wav","./assets/sounds/musicalCube/tracks/Daydreamer/melody/002_AHA_SV_70_guitar_stack_loop_daydreamer_Gm.wav","./assets/sounds/musicalCube/tracks/Daydreamer/melody/003_AHA_SV_70_brass_stack_loop_daydreamer_Gm.wav","./assets/sounds/musicalCube/tracks/Daydreamer/melody/004_AHA_SV_70_vocal_loop_daydreamer_Gm.wav"]
       },
       { 
         name : "In the City",
