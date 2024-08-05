a7f5ed866536e639004c8951ae21924938875a27
Paul Visciano <paulvisciano.dev@gmail.com>
2024-08-05 00:07:45 -0400

    Updates

---
+++ static/js/841.d553529e.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 841.d553529e.chunk.js.LICENSE.txt */
+"use strict";(globalThis.webpackChunkmusical_realms=globalThis.webpackChunkmusical_realms||[]).push([[841],{8841:(e,t,s)=>{s.r(t),s.d(t,{startStatusTap:()=>o});var n=s(5831),r=s(3743),a=s(1811);const o=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,n.wj)((()=>{const t=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(t/2,s/2);if(!o)return;const i=(0,r.a)(o);i&&new Promise((e=>(0,a.c)(i,e))).then((()=>{(0,n.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,r.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=841.d553529e.chunk.js.map
